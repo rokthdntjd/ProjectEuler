ff97a7ea59e70fc34e4b508d5150a5f4652dc90a
rokthdntjd <rokthdntjd@gmail.com>
2021-11-11 09:44:21 +0300

    Update Q18.py

--- Q18.py
+++ Q18.py
@@ -1,4 +1,4 @@
-a = '''
+sentence = '''
 75
 95 64
 17 47 82
@@ -15,4 +15,6 @@ a = '''
 63 66 04 68 89 53 67 30 73 16 69 87 40 31
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 '''
-print(a[1:3])
+import re
+sentence = re.sub(r"\s+", "", sentence, flags=re.UNICODE)
+print(sentence)
