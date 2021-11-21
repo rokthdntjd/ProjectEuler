a1c2fa27d3a0aafb6511a66885b9bb489791e08e
rokthdntjd <rokthdntjd@gmail.com>
2021-11-21 11:42:49 +0300

    da baby jhkscjb
    lol

--- Q12.py
+++ Q12.py
@@ -1,63 +1,17 @@
-n = 1
-An = 0
-
-def divisor_count(num):
-    global a
-    a = 0
-    for i in range(1, num):
-        if num % i == 0:
-            a += 1
-    return a
-
-while True:
-
-    An += n
-
-    divisor_count(An)
-
-    if a > 500:
-        print(An, n, a)
-        print(list)
-        break
-    print(An, n, a)
-    n += 1
-
-
-
-
-
-
-
-
-
-'''
 import math
 
-a = 12800
-c = 0
-max = 0
-maxn = 0
-maxa = 0
-
-while True:
-    c = 0
-    n = 0
-    for i in range(0, a+1):
-        n += int(i)
+def tri_num(n):
+    a = 0
     for i in range(1, n+1):
-        if n%i == 0:
-            c += 1
+        a += i
+    return a
 
-    if int(c) > max:
-        max = int(c)
-        maxn = n
-        maxa = a
+def ifsquare(n):
+    a = 1
+    while True:
+        if a^2 == n:
+            return True
+            break
+        a += 1
 
-    if int(c) > 500:
-        print("------------------------------")
-        print(a ,"|", n ,"|", c ,"|", max)
-        break
-
-    print(a ,"|", n ,"|", c ,"|", max ,"|", maxa ,"|", maxn)
-    a += 1
-'''
+print(ifsquare(10))
